2f48f7eace96dc0dae047bfd3915def3fa4b40e6
ellag7121 <ellag7121@wrdsb.ca>
2020-10-13 13:17:18 +0000

    finished a7q1! (again)

--- Main.java
+++ Main.java
@@ -1,21 +1,21 @@
 /**
- * asks user for grade, tells grade approximate letter grade
+ * gets a number grade, prints approximate letter grade
  * @author Graham Ellacott
  */
 public class Main {
 
   //a method that gets a grade and tells the user the letter grade
-  public static String examGrade(int grade){
+  public static void examGrade(int grade){
     if(grade >= 80){ //if grade is over 80
-      return "A"; //return A
+      System.out.println("A"); //print A
     }else if(grade >= 70){ //if grade is over 70
-      return "B"; //return B
+      System.out.println("B"); //print B
     }else if(grade >= 60){ //if grade is over 60
-      return "C"; //return C
+      System.out.println("C"); //print C
     }else if(grade >= 50){ //if grade is over 50
-      return "D"; //return D
+      System.out.println("D"); //print D
     }else{ //if grade is under 50
-      return "F"; //return F
+      System.out.println("F"); //print F
     }
   }
 
@@ -24,12 +24,10 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
     //check a bunch of different grades
     for(int i = 0; i < 100; i += 5){ //count by 5s from 0 to 99
-      String grade = examGrade(i); //find the letter grade
-      //print the grade to the screen
-      System.out.println(i + " = " + grade);
+      System.out.println(i); //print the number grade
+      examGrade(i); //find the letter grade
     }
     
   }
